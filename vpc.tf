cbcbd18f6e19d1120a50566ce05ccfb53a98aa0a
tkt <tktcorporation.go@gmail.com>
2020-08-09 02:51:19 +0900

    add vpc tf

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,21 @@
+variable "aws_region" {}
+
 provider "aws" {
   version = "~> 3.1"
+  region  = var.aws_region
 }
 
 variable "project_prefix" {}
 variable "vpc_cidr" {}
 
-resource "aws_vpc" "${var.project_prefix}-vpc" {
-  cidr_block       = "${var.vpc_cidr}"
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.project_prefix}"
+    Name = "${var.project_prefix}-vpc"
   }
 }
